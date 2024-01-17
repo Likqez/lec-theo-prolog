8636bc8b2828f1280a7b452f52f9696e791b9d46
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-01-17 15:26:37 +0100

    5_25.pl

---
+++ src/5_25.pl
@@ -0,0 +1,23 @@
+% script ex 5.25
+% 1
+killer(butch).
+
+%2
+married(marsellus,mia).
+married(mia,marsellus).
+
+%3
+dead(zed).
+
+%4
+massage(vincent, mia).
+
+kill(marsellus,X) :- massage(X,mia).
+dead(X) :- massage(X, Y).
+killer(X) :- kill(X, Y).
+
+%5
+loves(mia,X) :- dancer(X).
+
+%6
+eats(jules,X) :- jum(X);nutritiuous(X).
